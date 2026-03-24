c0210b48dd74b1d6da571df8bae9eb8822bcf3b6
Slim Masmoudi <smasmoudi@taec.com.mx>
2026-03-24 09:45:15 -0600

    chore: remove temp pdf reading scripts

--- astro-web/read_pdf.cjs
+++
@@ -1,8 +0,0 @@
-const fs = require('fs');
-const pdf = require('pdf-parse');
-
-let dataBuffer = fs.readFileSync('/Users/slimmasmoudi/taec-web/docs/Revisión sitio DDC 2026.pdf');
-
-pdf(dataBuffer).then(function(data) {
-    console.log(data.text);
-}).catch(console.error);
